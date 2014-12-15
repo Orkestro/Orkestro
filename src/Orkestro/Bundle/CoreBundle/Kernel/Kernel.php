af7daf40f18f9160ef0a993d861c215ffc973ede
velikan (home) <velikan55555@hotmail.com>
2014-12-15 04:02:08 +0300

    user bundle, templates updates fos user bundle rewrite to UserBundle backend/frontend templates update

--- src/Orkestro/Bundle/CoreBundle/Kernel/Kernel.php
+++ src/Orkestro/Bundle/CoreBundle/Kernel/Kernel.php
@@ -35,8 +35,8 @@ abstract class Kernel extends BaseKernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
 
             new Orkestro\Bundle\CoreBundle\OrkestroCoreBundle(),
             new Orkestro\Bundle\AddressingBundle\OrkestroAddressingBundle(),
---
+++ src/Orkestro/Bundle/UserBundle/Twig/SocialExposeExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Orkestro\Bundle\UserBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class SocialExposeExtension extends \Twig_Extension
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getGlobal()
+    {
+        return array(
+            'socials' => $this->container->getParameter('hwi_oauth.resource_owners'),
+        );
+    }
+
+    public function getName()
+    {
+        return 'social_expose_extension';
+    }
+}
