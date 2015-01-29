61b427af3d96c0c0a2be192a5239ccbe907fb9be
velikan (home) <velikan55555@hotmail.com>
2015-01-30 01:54:31 +0300

    temporary commit fixes

---
+++ src/Orkestro/Bundle/CoreBundle/Controller/AbstractBackendControllerInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Orkestro\Bundle\CoreBundle\Controller;
+
+use Knp\Component\Pager\Pagination\PaginationInterface;
+
+interface AbstractBackendControllerInterface
+{
+    public function getNamespace();
+    public function getModelClass();
+    public function getPaginationQuery();
+    public function getIndexListForms(PaginationInterface $pagination);
+
+    public function getModelRepository();
+}
